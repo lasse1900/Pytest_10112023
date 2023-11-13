094bcbc92ed48951809a345189ba59902b208b3d
lasse1900 <lauri.kyttala@gmail.com>
2023-11-13 13:19:11 +0200

    some commennting and added markers to pytest_challenge

--- pytest_challenge/test_write_and_read_numbers.py
+++ pytest_challenge/test_write_and_read_numbers.py
@@ -1,5 +1,7 @@
 # test_write_and_read.py
-# Please run script with command: [pytest -v -s test_write_and_read.py]
+# Windows / MacOs, please run script with command: [pytest -v -s test_write_and_read_numbers.py]
+# Ubuntu (22.04.3 LTS) please run script with command: [python3 -m pytest -v -s test_write_and_read_numbers.py]
+# With decorators command e.g. [pytest -s -v -m smoke] or [pytest -s -v -m half]
 
 import pytest
 from write_and_read_numbers import write_numbers_to_file, read_and_print_all_numbers, read_and_print_half_from_file
@@ -10,6 +12,7 @@ def numbers_file(tmpdir):
     write_numbers_to_file(filename)
     return filename
 
+@pytest.mark.smoke
 def test_read_and_print_all_numbers(capsys, numbers_file):
     read_and_print_all_numbers(numbers_file)
     captured = capsys.readouterr()
@@ -18,6 +21,8 @@ def test_read_and_print_all_numbers(capsys, numbers_file):
     print(expected_output)
     assert captured.out == expected_output
 
+@pytest.mark.smoke
+@pytest.mark.half
 def test_read_and_print_half_from_file(capsys, numbers_file):
     read_and_print_half_from_file(numbers_file)
     captured = capsys.readouterr()
